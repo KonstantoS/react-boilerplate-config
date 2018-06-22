1deb24e7eeaa831556e7fc812c898816407482aa
Kostiantyn Levytskyi <le.konstantos@gmail.com>
2018-06-22 15:40:34 +0300

    feat: some basic configuraton

---
+++ config/webpack.config.development.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const paths = require('./paths');
+
+module.exports = {
+  mode: 'development',
+  entry: paths.entryPoint,
+  output: {
+    path: paths.outputPath,
+    publicPath: '/',
+    filename: path.join('js', '[name].js'),
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+    modules: [paths.nodeModules, paths.src],
+    alias: {
+      config: paths.appConfig,
+    },
+  },
+  module: {
+    rules: [
+      {
+        use: 'file-loader',
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.svg$/,
+        exclude: [paths.publicFiles],
+        loader: 'svg-sprite-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              namedExport: true,
+              localIdentName: '[local]-[hash:base64:5]',
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: paths.scss,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(paths.publicFiles, 'index.html'),
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    port: process.env.PORT || 3000,
+  },
+};
